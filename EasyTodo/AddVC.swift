d9b237201a0fe8ba9336e3025e565f8c0e04a899
Thanapat Sorralump <thanapat423@gmail.com>
2018-02-21 00:37:40 +0700

    Initial Commit

---
+++ EasyTodo/AddVC.swift
@@ -0,0 +1,37 @@
+//
+//  AddVC.swift
+//  EasyTodo
+//
+//  Created by Thanapat Sorralump on 16/2/2561 BE.
+//  Copyright © 2561 Thanapat Sorralump. All rights reserved.
+//
+
+import UIKit
+
+protocol ListDelegate {
+  func sendData(list: String)
+}
+
+class AddVC: UIViewController {
+  
+  var delegate: ListDelegate?
+  
+  @IBOutlet weak var listField: UITextField!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  @IBAction func addList(_ sender: UIButton) {
+    guard let list = listField.text else { return }
+    delegate?.sendData(list: list)
+    
+    performSegue(withIdentifier: "unwindToMain", sender: self)
+  }
+}
+
+
+
+
+
+
---
+++ EasyTodo/MainTableViewController.swift
@@ -0,0 +1,51 @@
+//
+//  MainTableViewController.swift
+//  EasyTodo
+//
+//  Created by Thanapat Sorralump on 16/2/2561 BE.
+//  Copyright © 2561 Thanapat Sorralump. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController, ListDelegate {
+  
+  var lists: [String] = []
+  
+  @IBAction func unwindMain(segue: UIStoryboardSegue) { }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  func sendData(list: String) {
+    lists.append(list)
+    tableView.reloadData()
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if let destination = segue.destination as? AddVC {
+      destination.delegate = self
+    }
+  }
+}
+
+extension MainTableViewController {
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return lists.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "list", for: indexPath)
+    cell.textLabel?.text = lists[indexPath.row]
+    
+    return cell
+  }
+  
+  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    if editingStyle == .delete {
+      lists.remove(at: indexPath.row)
+      tableView.deleteRows(at: [indexPath], with: .fade)
+    }
+  }
+}
